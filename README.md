4d15b5164cdbb4e5e8a292ba9d00af631af9e31c
zts <zt-785@sjtu.edu.cn>
2024-03-17 15:58:49 +0800

    Done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # IMG-GC
+
+``pip install diffusers transformers accelerate scipy safetensors
+``
+
+``pip install -U bootstrap-flask``
+
+``
+pip install peft
+``
--- myweb/app.py
+++ myweb/app.py
@@ -113,19 +113,23 @@ class Img2textForm(FlaskForm):
     submit = SubmitField()
 def load_model_bilp():
     global processor,model_bilp
+    if model_bilp is not None:
+        return
     device_map = {'language_model': "cuda", \
                   'language_projection': 'cpu', \
                   'qformer': 'cpu', \
                   'query_tokens': 'cpu', \
                   'vision_model': 'cpu'}
-    modelpath = r"C:\Users\17612\Desktop\course-management\img2text"
-    processor,model_bilp = get_processor_and_model(modelpath=modelpath, device_map=device_map)
+    model_path = "Salesforce/blip2-opt-2.7b"
+    processor , model_bilp = get_processor_and_model(modelpath=model_path, device_map=device_map)
+    lora_fit(model_bilp)
 
 def load_pipe():
     global pipe
-    model_path = r"C:\Users\17612\Desktop\course-management\img2img"
+    if pipe is not None:
+        return
+    model_path = "stabilityai/stable-diffusion-2"
     if hasattr(torch.cuda, 'empty_cache'):
         torch.cuda.empty_cache()
-    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_path, torch_dtype=torch.float16, revision="fp16",
-                                                          use_auth_token=True,device_map='sequential')
-    # pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_path, device=device, use_auth_token=True)
+    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_path, torch_dtype=torch.float16, revision="fp16", use_auth_token=True , device_map='sequential')
+    # pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_path,  use_auth_token=True)
